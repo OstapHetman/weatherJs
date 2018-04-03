fed450d3c3e8a66da74031ac196cea0b0fb20278
OstapHetman <prickly4528@gmail.com>
2018-04-03 16:08:30 +0300

    display the weather

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Init weather object
 const weather = new Weather('Boston', 'MA');
+// Init UI
+const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -7,7 +9,7 @@ document.addEventListener('DOMContentLoaded', getWeather);
 function getWeather() {
     weather.getWeather({ mode: 'no-cors' })
         .then(results => {
-            console.log(results);
+            ui.paint(results);
         })
         .catch(err => console.log(err));
 }
